5b06e4aa4b244c42b046f6cd8d71791ee84eb831
Key <springfield0329@gmail.com>
2023-02-17 18:36:46 +0800

    feat: footer component done | footer 元件完成 (前台 layout完成)

--- src/components/front/FooterComponent.vue
+++ src/components/front/FooterComponent.vue
@@ -1,7 +1,140 @@
 <template>
-  <footer class="mt-20">
+  <footer
+    class="mt-20 border-t-4 border-primary-01 bg-primary-03 py-8 md:py-10"
+  >
     <div class="container">
-      <div>this is footer</div>
+      <div
+        class="flex flex-col items-center gap-4 md:flex-row md:items-stretch md:justify-between"
+      >
+        <div class="flex flex-col items-center lg:flex-row">
+          <h2 class="mb-4 md:mr-[134px] lg:mb-0">
+            <a
+              class="h-12 w-[196px] overflow-hidden whitespace-nowrap bg-[url('./images/logo/JoinSport03.png')] bg-cover bg-no-repeat indent-[101%] hover:opacity-75"
+              href="#"
+              >Join Sport 揪運動</a
+            >
+          </h2>
+          <div class="flex gap-[72px] text-left">
+            <div>
+              <p class="text-lg text-[#6F6F6F] md:mb-4">帳號</p>
+              <ul>
+                <li>
+                  <router-link
+                    class="leading-8 hover:text-primary-01 focus:text-secondary-blue"
+                    to=""
+                    >註冊</router-link
+                  >
+                </li>
+                <li>
+                  <router-link
+                    class="leading-8 hover:text-primary-01 focus:text-secondary-blue"
+                    to=""
+                    >登入</router-link
+                  >
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p class="text-lg text-[#6F6F6F] md:mb-4">揪團</p>
+              <ul>
+                <li>
+                  <router-link
+                    class="leading-8 hover:text-primary-01 focus:text-secondary-blue"
+                    to=""
+                    >參加揪團</router-link
+                  >
+                </li>
+                <li>
+                  <router-link
+                    class="leading-8 hover:text-primary-01 focus:text-secondary-blue"
+                    to=""
+                    >當主揪開團</router-link
+                  >
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p class="text-lg text-[#6F6F6F] md:mb-4">探索</p>
+              <ul>
+                <li>
+                  <router-link
+                    class="leading-8 hover:text-primary-01 focus:text-secondary-blue"
+                    to=""
+                    >熱門群組</router-link
+                  >
+                </li>
+                <li>
+                  <router-link
+                    class="leading-8 hover:text-primary-01 focus:text-secondary-blue"
+                    to=""
+                    >城市搜尋</router-link
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- icon -->
+        <div>
+          <div class="flex h-full flex-col justify-evenly lg:justify-between">
+            <ul class="mb-4 flex gap-6 md:gap-10">
+              <li>
+                <a
+                  href="#"
+                  class="flex h-12 w-12 items-center justify-center rounded-full bg-white hover:scale-90"
+                  ><img
+                    class=""
+                    src="../../assets/images/icon/Vector-1.png"
+                    alt=""
+                /></a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex h-12 w-12 items-center justify-center rounded-full bg-white hover:scale-90"
+                  ><img
+                    class=""
+                    src="../../assets/images/icon/twitter.png"
+                    alt=""
+                /></a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex h-12 w-12 items-center justify-center rounded-full bg-white hover:scale-90"
+                  ><img
+                    class=""
+                    src="../../assets/images/icon/Vector-2.png"
+                    alt=""
+                /></a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex h-12 w-12 items-center justify-center rounded-full bg-white hover:scale-90"
+                  ><img
+                    class=""
+                    src="../../assets/images/icon/envelope.png"
+                    alt=""
+                /></a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex h-12 w-12 items-center justify-center rounded-full bg-white hover:scale-90"
+                  ><img
+                    class=""
+                    src="../../assets/images/icon/Vector.png"
+                    alt=""
+                /></a>
+              </li>
+            </ul>
+            <p class="text-center text-xs text-[#3D3D3D] md:text-end">
+              Copyright© Join Sport All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,3 +1,30 @@
 <template>
   <h1>前台首頁</h1>
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
 </template>
