23c1f0c2b75771c0d34dde89ea175ff000844040
Key <springfield0329@gmail.com>
2023-03-10 18:49:41 +0800

    deploy

--- assets/AdminHomeView-c63384a0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as c,o as n}from"./index-5ea3d25b.js";const o={};function r(t,a){return n(),c("h1",null,"後台首頁")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/AdminOrdersView-daba7fac.js
@@ -0,0 +1,1 @@
+import{_ as e,a as r,o as c}from"./index-7d43ac81.js";const n={};function o(t,s){return c(),r("h1",null,"後台訂單頁面")}const _=e(n,[["render",o]]);export{_ as default};
