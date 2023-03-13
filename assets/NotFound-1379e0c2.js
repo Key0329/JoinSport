17f5d8bef075d835eab1f970168549d5af0e2ac5
Key <springfield0329@gmail.com>
2023-03-13 21:41:39 +0800

    deploy

--- assets/NotFound-1379e0c2.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,a as e,o as t}from"./index-2be02bfa.js";const n={name:"NotFound"};function a(c,r,s,_,p,u){return t(),e("h1",null,"404，你打錯網址囉~")}const f=o(n,[["render",a]]);export{f as default};
---
+++ assets/NotFound-c457142a.js
@@ -0,0 +1,1 @@
+import{_ as o,a as e,o as t}from"./index-dd70f97b.js";const n={name:"NotFound"};function a(c,r,s,_,p,u){return t(),e("h1",null,"404，你打錯網址囉~")}const f=o(n,[["render",a]]);export{f as default};
