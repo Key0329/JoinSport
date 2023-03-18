170fd705bb60c09802f7a73fb202a2edfda593a5
Key <springfield0329@gmail.com>
2023-03-18 15:43:17 +0800

    feat: 404 page | 更新 404 頁面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,5 +4,15 @@ export default {
 };
 </script>
 <template>
-  <h1>404，你打錯網址囉~</h1>
+  <div class="container">
+    <div class="flex h-screen flex-col items-center justify-center gap-6">
+      <div class="mx-auto w-[160px]">
+        <img src="../assets/images/feature/feature02.png" alt="404 img" />
+      </div>
+      <h1 class="text-4xl">Oops 404，打錯網址囉~</h1>
+      <RouterLink to="/" class="btn border hover:bg-primary-01 hover:text-white"
+        ><i class="pi pi-home mr-2"></i>點此回首頁</RouterLink
+      >
+    </div>
+  </div>
 </template>
